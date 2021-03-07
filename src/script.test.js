07b165149921ced6147d3cb85bc592afa31ce54e
ahmedbelal15 <ahmbelal95@gmail.com>
2021-03-08 01:54:11 +0200

    adding more tests

--- src/script.test.js
+++ src/script.test.js
@@ -10,7 +10,18 @@ it('this is a silly test', ()=>{
     expect('Hello').toEqual('Hello')
 })
 
-it('is Searching google', ()=>{
-    expect(googleSearch('ttteeesssttt', mockDb)).toEqual([])
-    expect(googleSearch('dog', mockDb)).toEqual(['dog.com', 'dogpictures.com'])
+describe('googleSearch', ()=>{
+    it('is Searching google', ()=>{
+        expect(googleSearch('ttteeesssttt', mockDb)).toEqual([])
+        expect(googleSearch('dog', mockDb)).toEqual(['dog.com', 'dogpictures.com'])
+    })
+    
+    it('works with undefined and null input',() =>{
+        expect(googleSearch(null, mockDb)).toEqual([])
+        expect(googleSearch(undefined, mockDb)).toEqual([])
+    })
+    
+    it("doesn't return more than 3 results", ()=>{
+        expect(googleSearch('.com', mockDb).length).toEqual(3)
+    })
 })
